e9c47cbd3d09a4f7fedbc5b461b54fe4abe31775
wur <609028727@qq.com>
2019-04-24 10:15:15 +0800

    update package.json

--- dist/electron/54.js
+++ dist/electron/54.js
@@ -1,1 +1,1 @@
-webpackJsonp([54],{188:function(a,b,c){"use strict";Object.defineProperty(b,"__esModule",{value:!0});var d=c(58),e=d(null,null,!1,null,null,null);b["default"]=e.exports}});
+webpackJsonp([54],{189:function(a,b,c){"use strict";Object.defineProperty(b,"__esModule",{value:!0});var d=c(58),e=d(null,null,!1,null,null,null);b["default"]=e.exports}});
